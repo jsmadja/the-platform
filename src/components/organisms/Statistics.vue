d4535e66958da61d425632a0e0953578f0522af5
jsmadja <xedy4bsa>
2020-04-10 21:10:39 +0200

    Initialize food quantity

--- src/components/organisms/Statistics.vue
+++ src/components/organisms/Statistics.vue
@@ -3,8 +3,10 @@
     <div>Statistics</div>
     <ul>
       <li>Number of people: {{ people.length }}</li>
+      <li>Platform floor: {{ platform.floor }}</li>
+      <li>Platform food quantity: {{ platform.foodQuantity }}</li>
       <li>
-        <b-table striped hover :items="items" sticky-header="300px"	small />
+        <b-table striped hover :items="items" sticky-header="300px" small />
       </li>
     </ul>
   </div>
@@ -13,10 +15,12 @@
 <script lang="ts">
 import { Component, Prop, Vue } from "vue-property-decorator";
 import { People } from "@/domain/people";
+import { Platform } from "@/domain/platform";
 
 @Component
 export default class Statistics extends Vue {
   @Prop() private people!: People;
+  @Prop() private platform!: Platform;
 
   data() {
     return {
